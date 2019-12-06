d6de20fe77f5a4f547768cd409c583aede1c8d05
Yernemm <yernemm@yernemm.xyz>
2019-12-06 14:53:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const SnoCord = require("./../SnoCord/index.js");
 const config = require("./config.json");
 
-let bot = new SnoCord.Bot(config);
+let bot = new SnoCord.Bot();
+bot.setConfig(config);
+
+bot.addResponse((message) => {return true;}, (message, respond) => {
+    console.log("oop")
+    respond("fuck")
+});
+bot.init();
