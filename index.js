ae0547ef74fed0dd0374023fbc01a7e60a5375d4
Yernemm <yernemm@yernemm.xyz>
2019-12-31 04:04:47 +0000

    yes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const SnoCord = require("./../SnoCord/index.js");
+const SnoDB = require("snodb");
 const config = require("./config.json");
+
+let db = new SnoDB('./db.sqlite')
 
 let bot = new SnoCord.Bot();
 bot.setConfig(config);
@@ -17,6 +20,25 @@ bot.addResponse(/^(fuck you gene)/i, (r) => r.respond("ay fuck you too"))
 bot.addCommand("help",(r)=>{r.respond(`I won't help you, ${r.message.author}`)})
 
 bot.addCommandHandler('./commands/');
+
+bot.addCommand('write', (r)=>{
+    let table = r.args[0]
+    let key = r.args[1]
+    let data = r.argsText.substring(table.length + key.length + 2);
+
+    db.setTo(table,key,data).then(
+        r.respond("Written.")
+    )
+})
+
+bot.addCommand('read', (r)=>{
+    let table = r.args[0]
+    let key = r.args[1]
+
+    db.getFrom(table,key).then(
+       (data=>{r.respond(data)})
+    )
+})
 
 
 
